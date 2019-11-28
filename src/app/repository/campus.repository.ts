3ed75599414b8cbe145a2dd2898f64a4de0e1c13
José Gabriel Gruber <jgabrielgruber@hotmail.com>
2019-11-27 22:13:27 -0300

    Corrigido estruturação; Implementado get;

--- src/app/repository/campus.repository.ts
+++ src/app/repository/campus.repository.ts
@@ -1,20 +1,30 @@
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 class Campus {
 
-    private nome: String
-    private id: String
-    private descricao: String
+	private nome: String
+	private id: String
+	private descricao: String
 
-    constructor(nome, descricao, id=null) {
-        this.nome = nome
-        this.descricao = descricao
-        this.id = id
-    }
+	constructor(nome, descricao, id = null) {
+		this.nome = nome
+		this.descricao = descricao
+		this.id = id
+	}
 
 }
 
 class CampusRepository {
-    
-    private list_campus: Array<Campus>
 
-    constructor() {}
+	private campus: Observable<Campus>
+	private collection: String = "campus"
+	private firestore: AngularFirestore
+
+	constructor() { }
+
+	public get(): Observable<Campus> {
+		this.campus = this.firestore.collection(this.collection).valueChanges()
+		return this.campus
+	}
+
 }
