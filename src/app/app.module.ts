3e07f9d0e2e06d0949ba88872c03a67041721ad0
Josnei Drosdek <josneidrosdek@gmail.com>
2019-11-28 21:36:15 -0300

    Reestruturado o Projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,8 +6,7 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './screen/app.component';
 import { Camera } from '@ionic-native/camera/ngx';
 import { IonicStorageModule } from '@ionic/storage';
 
@@ -18,7 +17,7 @@ import { AngularFirestore, AngularFirestoreModule } from '@angular/fire/firestor
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,
+  imports: [BrowserModule, IonicModule.forRoot(),
     IonicStorageModule.forRoot(), AngularFireModule.initializeApp(environment.config), // imports firebase/app needed for everything
     AngularFirestoreModule,
   ],
