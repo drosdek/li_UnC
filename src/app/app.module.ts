5e4ea13aab3459ebf340f8738a1f4858e530c13c
Josnei Drosdek <josneidrosdek@gmail.com>
2019-12-07 14:18:08 -0300

    Criado InfoWidget

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
+import { InfoWidget } from './widget/info.widget';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy, RouterModule } from '@angular/router';
 
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { IonicModule, IonicRouteStrategy, ModalController } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
@@ -17,7 +18,7 @@ import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
 	declarations: [
-		AppComponent,
+		AppComponent
 	],
 	entryComponents: [],
 	imports: [
